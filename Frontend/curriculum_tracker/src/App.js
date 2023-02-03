5f62e7522c0a18bc84228b334378fa1395cb84e2
Prajeesha <praji22@gmail.com>
2023-02-04 01:20:55 +0530

    Homepage
    Ictak homepage created and activated directions to login and signup page

--- Frontend/curriculum_tracker/src/App.js
+++ Frontend/curriculum_tracker/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' exact element={<Home/>}/>
-      <Route path='/login' exact = {<Login/>}/>
-      <Route path='/signup' exact = {<Signup/>}/>
+      <Route path='/login' exact element= {<Login/>}/>
+      <Route path='/signup' exact element= {<Signup/>}/>
     </Routes>
     </BrowserRouter>
   );

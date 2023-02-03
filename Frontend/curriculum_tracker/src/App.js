3a5d6c7be86f1a40c6655395be391a1bdf53c94c
NEETHU C <neethubalakrishna@gmail.com>
2023-02-04 01:32:32 +0530

    resolved

--- Frontend/curriculum_tracker/src/App.js
+++ Frontend/curriculum_tracker/src/App.js
@@ -9,17 +9,10 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' exact element={<Home/>}/>
-<<<<<<< HEAD
       <Route path='/login' exact = {<Login/>}/>
       <Route path='/signup' exact = {<Signup/>}/>
-     
-      
-
-    
-=======
       <Route path='/login' exact element= {<Login/>}/>
       <Route path='/signup' exact element= {<Signup/>}/>
->>>>>>> 5f62e7522c0a18bc84228b334378fa1395cb84e2
     </Routes>
     </BrowserRouter>
   );

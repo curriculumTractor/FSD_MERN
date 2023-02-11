cc0e8fdfe3f55af971703e347e6925ce320763fd
NEETHU C <neethubalakrishna@gmail.com>
2023-02-11 12:37:45 +0530

    commit

--- Frontend/curriculum_tracker/src/Components/Home/Home.jsx
+++ Frontend/curriculum_tracker/src/Components/Home/Home.jsx
@@ -4,113 +4,107 @@ import Navbar from './Navbar'
 const Home = () => {
   return (
     <div>
-       <Navbar/>
- 
-       <div className="row g-3">
-      
+    <Navbar/>
+      <div className="container-fluid">
+        <div className="row g-3">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                {/* Carousel starts */}
-                <div id="carouselExampleCaptions" class="carousel slide">
-                    <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              {/* Carousel starts */}
+              <div id="carouselExampleCaptions" class="carousel slide">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                </div>
                <div class="carousel-inner">
-                 <div class="carousel-item active">
+                  <div class="carousel-item active">
                     <img src={require('./HomeImages/Banner2.png')} class="d-block w-100" alt="..."/>
-                     <div class="carousel-caption d-none d-md-block">
-                     <h5>FullStack Development</h5>
-                     <p>Some representative placeholder content for the first slide.</p>
-                 </div>
-               </div>
-               <div class="carousel-item">
-                  <img src={require('./HomeImages/Banner3.png')} class="d-block w-100" alt="..."/>
-                  <div class="carousel-caption d-none d-md-block">
-                  <h5>DataScience & Analytics</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>FullStack Development</h5>
+                      <p>Some representative placeholder content for the first slide.</p>
+                    </div>
                   </div>
-               </div>
-               <div class="carousel-item">
-                  <img src={require('./HomeImages/Banner1.png')} class="d-block w-100" alt="..."/>
-                  <div class="carousel-caption d-none d-md-block">
-                  <h5>Cyber Security Analyst</h5>
-                  <p>Some representative placeholder content for the third slide.</p>
+                  <div class="carousel-item">
+                    <img src={require('./HomeImages/Banner3.png')} class="d-block w-100" alt="..."/>
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>DataScience & Analytics</h5>
+                      <p>Some representative placeholder content for the second slide.</p>
+                    </div>
                   </div>
-               </div>
-           </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-             <span class="visually-hidden">Next</span>
-            </button>
-          </div>
+                  <div class="carousel-item">
+                    <img src={require('./HomeImages/Banner1.png')} class="d-block w-100" alt="..."/>
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>Cyber Security Analyst</h5>
+                      <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
          {/* Carousel ends */}
             </div>
-        </div>
+          </div>
 
         <div className="row g-3 sectionbody">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                
-                <h4>Courses</h4>
-                  {/* card 1 */}
-                <div className="card">
-                  <img src={require('./HomeImages/19197641.jpg')} class="card-img-top" alt="..."/>
-                  <div className="card-body">
-                    <h5 className="card-title">Course Name</h5>
-                    <p className="card-text">Some quick example text to build on the card.</p>
-                    <a href="/" class="btn">Get Details</a>
-                  </div>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <h4>Courses</h4>
+                {/* card 1 */}
+              <div className="card">
+                <img src={require('./HomeImages/19197641.jpg')} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Course Name</h5>
+                  <p className="card-text">Some quick example text to build on the card.</p>
+                  <a href="/" class="btn">Get Details</a>
                 </div>
-
+              </div>
                 {/* card 2 */}
-                <div className="card">
-                  <img src={require('./HomeImages/20945347.jpg')} class="card-img-top" alt="..."/>
-                  <div className="card-body">
-                    <h5 className="card-title">Course Name</h5>
-                    <p className="card-text">Some quick example text to build on the card.</p>
-                    <a href="/" class="btn">Get Details</a>
-                  </div>
+              <div className="card">
+                <img src={require('./HomeImages/20945347.jpg')} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Course Name</h5>
+                  <p className="card-text">Some quick example text to build on the card.</p>
+                  <a href="/" class="btn">Get Details</a>
                 </div>
-
+              </div>
                 {/* card 3 */}
-                <div className="card">
-                  <img src={require('./HomeImages/20945659.jpg')} class="card-img-top" alt="..."/>
-                  <div className="card-body">
-                    <h5 className="card-title">Course Name</h5>
-                    <p className="card-text">Some quick example text to build on the card.</p>
-                    <a href="/" class="btn">Get Details</a>
-                  </div>
+              <div className="card">
+                <img src={require('./HomeImages/20945659.jpg')} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Course Name</h5>
+                  <p className="card-text">Some quick example text to build on the card.</p>
+                  <a href="/" class="btn">Get Details</a>
                 </div>
-
+              </div>
                 {/* card 4 */}
-                <div className="card">
-                  <img src={require('./HomeImages/20945659.jpg')} class="card-img-top" alt="..."/>
-                  <div className="card-body">
-                    <h5 className="card-title">Course Name</h5>
-                    <p className="card-text">Some quick example text to build on the card.</p>
-                    <a href="/" class="btn">Get Details</a>
-                  </div>
+              <div className="card">
+                <img src={require('./HomeImages/20945659.jpg')} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Course Name</h5>
+                  <p className="card-text">Some quick example text to build on the card.</p>
+                  <a href="/" class="btn">Get Details</a>
                 </div>
-             
+              </div>
             </div>
-        </div>
+          </div>
 
         <div className="row g-5">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <section className='footer text-center' style={{marginTop:"20%"}}>
-                  <img src={require('./HomeImages/cropped-ict-ico.png')} alt="" />
-                  <p>GF-1 Thejaswini Building, Technopark Rd, Thiruvananthapuram, Kerala 695581<br/>
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <section className='footer text-center' style={{marginTop:"20%"}}>
+              <img src={require('./HomeImages/cropped-ict-ico.png')} alt="" />
+                <p>GF-1 Thejaswini Building, Technopark Rd, Thiruvananthapuram, Kerala 695581<br/>
                   ICT Academy of Kerala is supported by Govt. of India , partnered by Govt. of Kerala and the IT industry.<br/>
                   Copyright © 2023 ICT Academy of Kerala.  All rights reserved.</p>
-                </section>
+              </section>
             </div>
+          </div>
         </div>
-    </div>
-   
+  </div>
   )
 }
 
--- Frontend/curriculum_tracker/src/Components/Home/Navbar.jsx
+++ Frontend/curriculum_tracker/src/Components/Home/Navbar.jsx
@@ -3,51 +3,50 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-    <nav class="navbar fixed-top " >
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src={require('../imagelogo/logo_ct.png')}  alt="..." /></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Curriculum Tracker</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      <nav class="navbar fixed-top " style={{background:"#CDC4F9"}} >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src={require('../imagelogo/logo_ct.png')}  alt="..." /></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Curriculum Tracker</h5>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./login">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./signup">Signup</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./login">Curriculum</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Category</a>
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li><a class="dropdown-item" href="#">Retail</a></li>
+                    <li><a class="dropdown-item" href="#">Academic</a></li>
+                    <li><a class="dropdown-item" href="#">Corporate</a></li>
+                  </ul>
+                </li>
+              </ul>
+            <form class="d-flex mt-3" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button class="btn btn-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./login">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./signup">Signup</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./login">Curriculum</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Category
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Retail</a></li>
-              <li><a class="dropdown-item" href="#">Academic</a></li>
-              <li><a class="dropdown-item" href="#">Corporate</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
+    </nav>
   </div>
-</nav>
-</div>
   )
 }
 

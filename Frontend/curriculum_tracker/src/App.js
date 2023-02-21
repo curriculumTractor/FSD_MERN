54dcf14c10af4aebc9142b1e40ad9a4e1f83d94a
NEETHU C <neethubalakrishna@gmail.com>
2023-02-21 20:53:57 +0530

    app update

--- Frontend/curriculum_tracker/src/App.js
+++ Frontend/curriculum_tracker/src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 import Admin from './Components/Admindashboard/Admin';
 import User from './Components/Userdashboard/User';
-import React from 'react';
-
-
 import Profile from './Components/Profile/Profile';
+import Aprofile from './Components/Profile/Aprofile';
 import Requirement from './Components/RequirementForm/Requirement';
 import RecieveReqrmnt from './Components/Userdashboard/Requirement/RecieveReqrmnt';
 import Search from './Components/Search/Search';
-import Aprofile from './Components/Profile/Aprofile';
 import CurriculumDisplay from './Components/Curriculum/CurriculumDisplay';
-
-
-
 import ViewCurriculm from './Components/ViewAll_curriculm/ViewCurriculm';
 import ViewallFaculty from './Components/ViewAll_curriculm/ViewallFaculty';
 import Fileupload from './Components/Userdashboard/FileUplod/Fileupload';
-import Signin from './Components/Login/Signin';
+
+
 
 
 
@@ -33,9 +30,8 @@ function App() {
         <Route path='/login' exact element={<Login />} />
         <Route path='/signup' exact element={<Signup />} />
         <Route path='/search' exact element={<Search/>} />
-        <Route path='/signin' exact element={<Signin/>}/>
-        
 
+        
         <Route path='/admin' exact element={<Admin />} />
         <Route path='/aprofile' exact element={<Aprofile />} />
         <Route path='/requirement' exact element={<Requirement />} />
@@ -43,19 +39,14 @@ function App() {
         <Route path='/viewcurriculm' exact element={<ViewCurriculm/>} />
 
 
-
         <Route path='/user' exact element={<User />} />
-        <Route path='/profile' exact element={<Profile />} />
+        <Route path='/uprofile' exact element={<Profile />} />
         <Route path='/recvrqrmnt' exact element={<RecieveReqrmnt/>} />
         <Route path='/viewallfaculty' exact element={<ViewallFaculty/>} />
         <Route path='/fileupload' exact element ={<Fileupload/>}/>
        
        
 
-      
-        
-
-      
       </Routes>
     </BrowserRouter>
   );

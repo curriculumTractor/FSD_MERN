d6a9a8ab41cb28351132b658a1fe601fbf5cf55c
NEETHU C <neethubalakrishna@gmail.com>
2023-02-22 13:07:37 +0530

    login

--- Frontend/curriculum_tracker/src/Components/Login/Login.jsx
+++ Frontend/curriculum_tracker/src/Components/Login/Login.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
 import axios from 'axios'
 import styles from "./styles.module.css"
 
 const Login = () => {
-
-	
 
 	const [credentials, setCredentials] = useState(
         {
@@ -14,9 +13,8 @@ const Login = () => {
          role: ""
         })
         let role = ["admin", "user"]
-       const navigate =useNavigate();
+        const navigate =useNavigate();
      
-
         const handleSubmit = async (e) => {
             e.preventDefault();
             const response = await fetch("http://localhost:3005/login", {
@@ -44,24 +42,7 @@ const Login = () => {
                 toast.error("invalid credentials");
             }
         }
-        //  if  (json.success && role === "admin") {
-        //     //save the auth token and redirect
-        //     localStorage.setItem('token', json.token);
-        //     navigate("/admin");
-        //     toast.success("login Successfully");
-        // }
-        // else if  (json.success && role === "user") {
-        //      //save the auth token and redirect
-        //      localStorage.setItem('token', json.token);
-        //      navigate("/user");
-        //     //  toast.success("login Successfully")
-        // }
-         
-    //     else{
-    //         toast.error("invalid credentials");
-    //     }
-    // }
-
+       
     const onChange = (e) => {
         setCredentials(
             { ...credentials,
@@ -72,44 +53,37 @@ const Login = () => {
   return (
     
 	<div className={styles.login_container}>
-	<div className={styles.login_form_container}>
-		<div className={styles.left}>
-			<form className={styles.form_container} onSubmit={handleSubmit}>
-				<h1>Login to Your Account</h1>
-
-				<div>
-                    <select className="form-select form-select-lg" required={true} id="role" name="role" value={credentials.role}  onChange={onChange} style={{ backgroundColor: "aliceblue", fontWeight: "500" }}>
-                        <option defaultValue >Select Role</option>
-                        <option value="admin">Admin</option>
-                        <option value="employee">User</option>
-                       
-                    </select>
-                  </div>
-
-
-				<input
-					type="email"
-					placeholder="Email"
-					name="email"
-					value={credentials.email} 
-					onChange={onChange}
-					
-					required
-					className={styles.input}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					name="password"
-					value={credentials.password} 
-					onChange={onChange} 
-					
-					required
-					className={styles.input}
-				/>
-				
+	    <div className={styles.login_form_container}>
+		    <div className={styles.left}>
+			    <form className={styles.form_container} onSubmit={handleSubmit}>
+				    <h1>Login to Your Account</h1>
+                    <div>
+                        <select className="form-select form-select-lg" required={true} id="role" name="role" value={credentials.role}  onChange={onChange} style={{ backgroundColor: "aliceblue", fontWeight: "500" }}>
+                            <option defaultValue >Select Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="employee">User</option>
+                        </select>
+                    </div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        value={credentials.email} 
+                        onChange={onChange}
+                        required
+                        className={styles.input}
+				    />
+				    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={credentials.password} 
+                        onChange={onChange} 
+                        required
+                        className={styles.input}
+				    />
 				<button type="submit" className={styles.green_btn} >
-					Sing In
+					Sign In
 				</button>
 			</form>
 		</div>

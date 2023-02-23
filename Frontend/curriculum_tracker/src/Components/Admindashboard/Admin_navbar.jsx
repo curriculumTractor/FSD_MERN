f2b3cc4f90b8d1ec0ff2b6dada13bfe68c985147
NEETHU C <neethubalakrishna@gmail.com>
2023-02-23 11:43:19 +0530

    hhh

--- Frontend/curriculum_tracker/src/Components/Admindashboard/Admin_navbar.jsx
+++ Frontend/curriculum_tracker/src/Components/Admindashboard/Admin_navbar.jsx
@@ -11,10 +11,10 @@ const Admin_navbar = () => {
                     <a class="navbar-brand" href="/admin"><img src={require('../imagelogo/logo_ct.png')}  alt="..." /></a>
                     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0" style={{paddingRight:"5%"}}>
-
-                        <li class="nav-item">
+                            <li class="nav-item">
                                 <a class="nav-link" href="#"><input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" /></a>
                             </li>
+<<<<<<< Updated upstream
 
    
                             {/* Profile */}
@@ -32,8 +32,20 @@ const Admin_navbar = () => {
 
            
 
+=======
+                            <li class="nav-item dropdown">
+                                <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa fa-user"></i>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-end">
+                                    <li><a class="dropdown-item" href="/admin"style={{color:"#2b2c2c"}}>Dashbord</a></li>
+                                    <li><a class="dropdown-item" href="/aprofile" style={{color:"#2b2c2c"}}>My Profile</a></li>
+                                    <li><a class="dropdown-item" href="/admin"style={{color:"#2b2c2c"}}>Settings</a></li>
+                                    <li><a class="dropdown-item" href="/"style={{color:"#2b2c2c"}}>Logout</a></li>
+                                </ul>
+                            </li>
+>>>>>>> Stashed changes
                         </ul>
-
                     </div>
                 </div>
             </nav>

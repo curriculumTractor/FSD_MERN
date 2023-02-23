324a66cf6b777851fa896911cd1379b72b33c2b1
NEETHU C <neethubalakrishna@gmail.com>
2023-02-23 11:48:49 +0530

    admin nav update

--- Frontend/curriculum_tracker/src/Components/Admindashboard/Admin_navbar.jsx
+++ Frontend/curriculum_tracker/src/Components/Admindashboard/Admin_navbar.jsx
@@ -14,29 +14,9 @@ const Admin_navbar = () => {
                             <li class="nav-item">
                                 <a class="nav-link" href="#"><input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" /></a>
                             </li>
-<<<<<<< Updated upstream
-
-   
-                            {/* Profile */}
-              <li class="nav-item dropdown">
-                <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fa fa-user"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-end">
-                <li><a class="dropdown-item" href="/admin"style={{color:"#2b2c2c"}}>Dashbord</a></li>
-                  <li><a class="dropdown-item" href="/aprofile" style={{color:"#2b2c2c"}}>My Profile</a></li>
-                  <li><a class="dropdown-item" href="#"style={{color:"#2b2c2c"}}>Settings</a></li>
-                  <li><a class="dropdown-item" href="/"style={{color:"#2b2c2c"}}>Logout</a></li>
-                </ul>
-                </li>
-
-
-           
-
-=======
                             <li class="nav-item dropdown">
                                 <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa fa-user"></i>
-                                </a>
+                                    <i class="fa fa-user"></i></a>
                                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-end">
                                     <li><a class="dropdown-item" href="/admin"style={{color:"#2b2c2c"}}>Dashbord</a></li>
                                     <li><a class="dropdown-item" href="/aprofile" style={{color:"#2b2c2c"}}>My Profile</a></li>
@@ -44,7 +24,6 @@ const Admin_navbar = () => {
                                     <li><a class="dropdown-item" href="/"style={{color:"#2b2c2c"}}>Logout</a></li>
                                 </ul>
                             </li>
->>>>>>> Stashed changes
                         </ul>
                     </div>
                 </div>
